95f99d396e2e1e5a1a6459a8bb1f0b579153ffa7
Lizeth-Del-Rio <liz-delrio@outlook.com>
2021-09-02 09:20:11 -0500

    organizacion de archivos 2.0

--- src/lib/fireFunctions.js
+++ src/lib/fireFunctions.js
@@ -1,10 +1,3 @@
-// aqui exportaras las funciones que necesites
-
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!')
-}
-
 export const loguinGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider()
   firebase.auth()
@@ -33,44 +26,7 @@ export const loguinGoogle = () => {
     })
 }
 
-export const emailPass = () => {
-  const email1 = document.getElementsByClassName('input-register')[1].value
-  const password1 = document.getElementsByClassName('input-register')[3].value
-  const email2 = document.getElementsByClassName('input-register')[2].value
-  const password2 = document.getElementsByClassName('input-register')[4].value
-  const passRules = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,16}$/.test(password1)
-
-  const errorP = document.getElementById('error-p')
-
-  if (email1 === '' || email2 === '' || password1 === '' || password2 === '') {
-    errorP.style.display = 'block'
-    errorP.textContent = 'Hay campos vacios'
-    setTimeout(() => { errorP.style.display = 'none' }, 3500)
-  } else {
-    if (passRules) {
-      if (email1 === email2 && password1 === password2) {
-        register(email1, password1)
-      } else {
-        if (email1 !== email2) {
-          errorP.style.display = 'block'
-          errorP.textContent = 'El email y su confirmacion deben coincidir'
-          setTimeout(() => { errorP.style.display = 'none' }, 4500)
-        }
-        if (password1 !== password2) {
-          errorP.textContent = 'La contraseña y su confirmacion deben coincidir'
-          errorP.style.display = 'block'
-          setTimeout(() => { errorP.style.display = 'none' }, 4500)
-        }
-      }
-    } else {
-      errorP.textContent = 'Contraseña incorrecta'
-      errorP.style.display = 'block'
-      setTimeout(() => { errorP.style.display = 'none' }, 3500)
-    }
-  }
-}
-
-const register = (email, password, names) => {
+export const register = (email, password, names) => {
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in
