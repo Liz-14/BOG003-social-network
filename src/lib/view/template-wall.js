7346a980a10039af23953dde7799f32d98b6dbea
Lizeth-Del-Rio <liz-delrio@outlook.com>
2021-09-03 17:28:50 -0500

    actualizacion estilos muro mobile

--- src/lib/view/template-wall.js
+++ src/lib/view/template-wall.js
@@ -4,7 +4,17 @@ export const templateWall = () => {
   const wall = `
   <header>
     <button type="button" id="btn-logout"><a href="#/"><img src="img/btnLogout.png" alt="logo" id="img-logout"></a></button>
-    <a href="#/" id = "a-logo"><img src="img/Logo.png" alt="logo" id="logo-w"></a>`
+    <a href="#/" id = "a-logo"><img src="img/Logo.png" alt="logo" id="logo-w"></a>
+  </header>
+
+  <nav>
+    <ul>
+      <li> <button type="button" id="btn-home"> <a href="#"> <img src="img/home.png" alt="logo" class="img-btn-nav"> </a> </button> </li>
+      <li> <button type="button" id="btn-publish"> <a href="#"> <img src="img/publish.png" alt="logo" class="img-btn-nav"> </a> </button> </li>
+      <li> <button type="button" id="btn-perfil"> <a href="#"> <img src="img/perfil.png" alt="logo" class="img-btn-nav" id="img-perfil"> </a> </button> </li>
+    </ul>
+  </nav>
+  `
 
   const sectionW = document.createElement('section')
   sectionW.id = 'w-container'
